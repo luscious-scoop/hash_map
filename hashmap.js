bc7647500cea20b3f6a2af68a1365fc7061cbe2d
Haseeb <ah085904@gmail.com>
2026-09-09 16:02:52 +0500

    modulo hashcode each iteration instead of after the loop

--- hashmap.js
+++ hashmap.js
@@ -11,8 +11,9 @@ function HashMap() {
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % capacity;
     }
 
-    return hashCode % capacity;
+    return hashCode;
   }
 }
