a306fd820cb136e3fc800076aa4826b5cac5e7e6
Haseeb <ah085904@gmail.com>
2026-09-09 14:40:59 +0500

    write a hashing function

--- hashmap.js
+++ hashmap.js
@@ -5,6 +5,14 @@ function HashMap() {
   let buckets = new Array(capacity);
   buckets.fill([]);
 
-  console.log(buckets.length);
+  function hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
 }
-HashMap();
